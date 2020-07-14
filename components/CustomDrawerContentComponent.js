bdf79362e81597019516ba13fa65e97adc2d018d
Pavin Mathew <pvmathew@ucdavis.edu>
2020-07-14 19:21:54 +0900

    Added currentUser to Drawer Menu

--- components/CustomDrawerContentComponent.js
+++ components/CustomDrawerContentComponent.js
@@ -4,18 +4,31 @@ import {
   DrawerItemList,
   DrawerItem,
 } from "@react-navigation/drawer";
+import { Text } from "react-native";
 import { logoutUser } from "../redux/actions";
 import { connect } from "react-redux";
+import Firebase from "firebase";
 
 function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
+      <Text style={{ textAlign: "right", marginRight: 10 }}>Hi there,</Text>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 15,
+          marginBottom: 10,
+          marginRight: 10,
+          textAlign: "right",
+        }}
+      >
+        {Firebase.auth().currentUser.email}
+      </Text>
       <DrawerItemList {...props} />
       <DrawerItem label="Logout" onPress={() => props.logoutUser()} />
     </DrawerContentScrollView>
   );
 }
-
 
 const mapStateToProps = (state) => ({});
 
